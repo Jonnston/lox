77033eb5b63913cf8fc8caade6bb6ebbebcd0813
Jonnston <dominickjohnston@yahoo.com>
2021-09-11 18:29:15 -0400

    Generated first=pass expression classes.

--- com/craftinginterpreters/tool/GenerateAst.java
+++ com/craftinginterpreters/tool/GenerateAst.java
@@ -23,12 +23,13 @@ public class GenerateAst {
     private static void defineAst(String outputDir, String baseName,
             List<String> types)
             throws IOException {
-        String path = outputDir + "/" + baseName + ".java";
+        String path = outputDir + System.getProperty("file.separator") + 
+            baseName + ".java";
         PrintWriter writer = new PrintWriter(path, "UTF-8");
 
-        writer.println("package com.craftinginterpreters.lox");
+        writer.println("package com.craftinginterpreters.lox;");
         writer.println();
-        writer.println("import java.util.List");
+        writer.println("import java.util.List;");
         writer.println();
         writer.println("abstract class " + baseName + "{");
 
